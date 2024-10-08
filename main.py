5a45bc5866b1a6affce17d866a44778e3eccf6f1
cygnusdelta <Dalton.royals@hotmail.com>
2024-10-08 18:41:54 -0500

    added player sprite and player movement logic

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import pygame
 from constants import *
+from player import *
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH /2
+    y = SCREEN_HEIGHT / 2
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    player = Player(x, y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
+        for object in updateable:
+            object.update(dt)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 if __name__ == "__main__":
     main()
