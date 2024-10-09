b225d650c56c269fb686cc1299377ba6c0b8ba1c
cygnusdelta <Dalton.royals@hotmail.com>
2024-10-08 19:27:01 -0500

    added asteroid movement and spawning logic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from player import *
+from asteroidfield import *
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -13,8 +14,12 @@ def main():
     y = SCREEN_HEIGHT / 2
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updateable, drawable)
+    Asteroid.containers = (updateable, drawable, asteroids)
+    AsteroidField.containers = (updateable)
     player = Player(x, y)
+    asteroid_field = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
